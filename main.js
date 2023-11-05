73e1f80e7109f79a709fae78b8c7a51dae26edf7
thelastenvoy <satvikbatra@gmail.com>
2023-11-05 16:19:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
 const fs = require('fs');
+const csv = require('csv-parser');
 
-const jsonFilePath = process.argv[2];
+const csvFilePath = process.argv[2];
+const columnName = process.argv[3];
 
-fs.readFile(jsonFilePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(`Error reading file: ${err}`);
-    return;
-  }
+let sum = 0;
+let count = 0;
 
-  const users = JSON.parse(data);
-
-  // TODO: Perform the required operations on the users data
-
-  // Print the total number of users
-  console.log(`Total number of users: ${users.length}`);
-
-  // Find the user with the highest score and print their details
-  const highestScoreUser = users.reduce((prev, current) => (prev.score > current.score ? prev : current));
-  console.log('User with the highest score:', highestScoreUser);
-
-  // Sort the users based on their scores in descending order
-  users.sort((a, b) => b.score - a.score);
-
-  // Write the sorted data back to the JSON file
-  fs.writeFile(jsonFilePath, JSON.stringify(users, null, 2), (err) => {
-    if (err) {
-      console.error(`Error writing file: ${err}`);
-      return;
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on('data', (data) => {
+    const value = Number(data[columnName]);
+    if (!isNaN(value)) {
+      sum += value;
+      count++;
     }
-    console.log('Data sorted and written back to the JSON file.');
+  })
+  .on('end', () => {
+    if (count > 0) {
+      const average = sum / count;
+      console.log(`The average value of ${columnName} is: ${average}`);
+    } else {
+      console.log(`No valid values found in the ${columnName} column.`);
+    }
+  })
+  .on('error', (err) => {
+    console.error(`Error reading CSV file: ${err}`);
   });
-});
